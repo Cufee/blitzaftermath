8346dbb7700fa0c7c6f4abb11ea5becb19f14dd6
Vlad Kouzin <vkouzin@outlook.com>
2020-08-07 16:04:36 -0700

    added cache updates twice daily

--- cache_workers/stats_data_cache.py
+++ cache_workers/stats_data_cache.py
@@ -2,5 +2,34 @@ import requests
 import rapidjson
 
 from cogs.api.mongoApi import StatsApi
+from pymongo import MongoClient
 
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
+
+
+client = MongoClient("mongodb://51.222.13.110:27017")
 Api = StatsApi()
+players_db = client.stats.players
+
+
+def run():
+    player_list = list(players_db.find())
+    print(len(player_list))
+    if len(player_list) > 100:
+        for i in range(0, len(player_list), 100):
+            statsApi.update_stats(player_list[i:i + n])
+    else:
+        statsApi.update_stats(player_list)
+
+
+if __name__ == "__main__":
+    scheduler = BlockingScheduler()
+    scheduler.add_job(run, CronTrigger.from_crontab('0 10 * * *'))
+    scheduler.add_job(run, CronTrigger.from_crontab('0 12 * * *'))
+    print('Press Ctrl+{0} to exit'.format('C'))
+
+    try:
+        scheduler.start()
+    except (KeyboardInterrupt, SystemExit):
+        pass
