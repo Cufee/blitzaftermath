7f610a7417e942601c7f7cc51969367edb6537c1
Cufee <vkouzin@outlook.com>
2020-11-02 14:02:07 -0800

    replaced bot

--- bot.py
+++ bot.py
@@ -1,11 +1,10 @@
 import rapidjson
 import os
-
+import discord
 import discord
 from discord.ext import commands, tasks
 from itertools import cycle
 from cogs.core_logger.logger import Logger
-
 logger = Logger()
 
 # Startup
@@ -15,41 +14,44 @@ TOKEN = settings["TOKEN"]
 mode = settings["mode"]
 prefix = settings["prefix"]
 default_game = settings["default_game"]
-client = commands.Bot(command_prefix=prefix, case_insensitive=False, activity=discord.Game(name=default_game))
+client = commands.Bot(command_prefix=prefix, case_insensitive=True)
+
+
+async def settings_parser():
+    with open(f'{os.path.dirname(os.path.realpath(__file__))}/cogs/core_multi_guild/cache/feature_list.json') as settings_json:
+        settings = rapidjson.load(settings_json)
+    return settings
 
 
 # Startup
 @client.event
 async def on_ready():
     logger.log(f'{client.user.name} online!')
-    # settings = await settings_parser()
-    cnt = 0
+    settings = await settings_parser()
     for filename in os.listdir(f'{os.path.dirname(os.path.realpath(__file__))}/cogs'):
-        cnt += 1
         if filename.endswith('.py'):
             if filename.startswith('core_'):
                 client.load_extension(f'cogs.{filename[:-3]}')
                 logger.log(f'{filename[:-3]} was loaded')
                 continue
-            # cog_settings = settings.get(filename[:-3])
-            # cog_name = cog_settings.get('name')
-            # is_enabled = cog_settings.get(f'enabled_{mode}')
-            is_enabled = True  # Need to replace once hooked up to db
+            cog_settings = settings.get(filename[:-3])
+            cog_name = cog_settings.get('name')
+            is_enabled = cog_settings.get(f'enabled_{mode}')
             if is_enabled:
                 client.load_extension(f'cogs.{filename[:-3]}')
-                logger.log(f'{filename} was loaded')
+                logger.log(f'{cog_name} was loaded')
             else:
-                logger.log(f'{filename} is disabled')
-    print(cnt)
+                logger.log(f'{cog_name} is disabled')
+
+# Tasks
+# Root tasks go here
+
+
+# Root Commands
+# Commands go here
+
 
 # Cog managment
-@client.command(hidden=True)
-@commands.is_owner()
-async def info(ctx):
-    await ctx.message.delete()
-    await ctx.send(f'Message: {ctx.message.id} [{ctx.message.channel}: {ctx.message.channel.id}]\nGuild: {ctx.message.guild.id}', delete_after=10)
-
-
 @client.command(hidden=True)
 @commands.is_owner()
 async def load(ctx, extension):
